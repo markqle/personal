d2d13d3d84932b3b0b16eca969b83c40517f908b
Mark Le <88472632+markqle@users.noreply.github.com>
2025-03-25 22:40:52 -0700

    Update vite 1.0

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/personal",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/personal",
 });
